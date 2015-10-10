fcd4d9ece91f32c1ec4caa926e262aee652bfc21
mashamm <marija.samojlenko@gmail.com>
2015-10-10 06:19:01 +0300

    prop

--- src/main/java/config/Application.java
+++ src/main/java/config/Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:application.properties")
+@PropertySource("classpath:apllication.properties")
 public class Application {
 	
     public static void main(String[] args) {
