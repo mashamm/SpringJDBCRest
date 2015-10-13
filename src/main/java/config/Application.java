7d844397a1197b1e171b6fb7c0a337618c9d6472
mashamm <marija.samojlenko@gmail.com>
2015-10-13 11:13:21 +0300

    fix DataSourceAutoConfig

--- src/main/java/config/Application.java
+++ src/main/java/config/Application.java
@@ -2,10 +2,14 @@ package config;
 
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
 @PropertySource("classpath:aplication.properties")
 public class Application {
 	
