8f31b38ae3344f3b033b9e321d3756311aacf3e9
mashamm <marija.samojlenko@gmail.com>
2015-10-27 17:07:00 +0200

    add more tests

--- src/test/java/test/restservices/ControllerTest.java
+++ src/test/java/test/restservices/ControllerTest.java
@@ -2,15 +2,14 @@ package test.restservices;
 
 
 import static org.junit.Assert.*;
-
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.xpath;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+import java.util.List;
+import static java.util.Arrays.asList;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -23,7 +22,6 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-
 import model.lead;
 
 @WebAppConfiguration
@@ -47,12 +45,12 @@ public class ControllerTest {
 	    public void getLeadById() throws Exception {
 	        String jsonData = mockMvc
 	        		.perform(get("/leads/{id}")
-	                .param("id", "77")
+	                .param("id", "77"))
 	                .andExpect(status().isOk())
 	                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
 	                .andReturn()
 	                .getResponse()
-	                .getContentAsString());
+	                .getContentAsString();
 
 	        lead lead = JsonUtil.readObject(jsonData, lead.class);
 
@@ -60,5 +58,50 @@ public class ControllerTest {
 	        assertEquals("IDs aren't match", 77,lead.getId().longValue());
 	        assertNotNull("Name can't be empty", lead.getName());
 	        
+	    }
+	    @Test
+	    public void getAll() throws Exception 
+	    {	 String jsonData = mockMvc
+	                 .perform(get("/leads/all"))
+	                 .andExpect(status().isOk())
+	                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+	                 .andReturn()
+	                 .getResponse()
+	                 .getContentAsString();
+	    	List<lead> leads = asList(JsonUtil.readObject(jsonData, lead[].class));
+	        	 assertFalse(leads.isEmpty());
+      
+	        	    }
+	    @Test
+	    public void create()throws Exception
+	    {
+	    	String jsonData=mockMvc
+	    			.perform(post("/leads/create/{name}")
+	    			.param("name","TestMockName")
+	    			.param("info","TestInfo"))
+	    			.andExpect(status().isOk())
+	    			.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+	    			.andReturn()
+	    			.getResponse()
+	    			.getContentAsString();
+	    	
+	    	Long idcreated=JsonUtil.readObject(jsonData, lead.class).getId();
+	    	
+	    	assertNotNull("Lead id cann't be a null",idcreated);
+	    }
+	    @Test
+	    public void update() throws Exception
+	    {
+	    	String jsonData=mockMvc
+	    			.perform(put("/leads/update/{id}")
+	    			.param("id", "82","name","NameUpdated","info","InfoUpdated"))
+	    			.andExpect(status().isOk())
+	    			.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+	    			.andReturn()
+	    			.getResponse()
+	    			.getContentAsString();
+	    	
+	    	Long idupdated= JsonUtil.readObject(jsonData, lead.class).getId();
+	    	assertNotNull("Lead id cannot be a null",idupdated);
 	    }
 }
